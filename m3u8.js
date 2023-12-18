3c57b3e851b1a58f1a317d5b8ee8be3e192894ad
atong <100357522+conghua11@users.noreply.github.com>
2023-12-18 19:56:48 +0800

    Update m3u8.js

--- m3u8.js
+++ m3u8.js
@@ -2,7 +2,7 @@
 
 ^http[s]?:\/\/(h5play|10play|120play|long|kkk520)\.(snowing|beibeini3|pili01a|fuhuida|riyufanyi|hncke).*\/(vedio|videos.|watch|static|upload|watch[0-9]+)\/.*\/.*\.m3u8\?auth_key.*$ url script-request-header https://raw.githubusercontent.com/conghua11/QuantumultX/main/m3u8.js
 # 通杀所有
-^(https?:\/\/)(?!long)([^./]+\.)*(longyuandingyi.com|zihzot.com|snowing.ren|vzcnqmr.cn|beibeini3.cn|pili01a.net|hhclyjz.cn|bhhcdou.cn|xuxivwx.cn|sdtuxyh.cn|kngpdsz.cn|sugugaj.cn|mqqjrv.com)(\/.*)? url 302 $1long.$3$4
+^(https?:\/\/)(?!long|kkk520)([^./]+\.)*(hncke.top|longyuandingyi.com|zihzot.com|snowing.ren|vzcnqmr.cn|beibeini3.cn|pili01a.net|hhclyjz.cn|bhhcdou.cn|xuxivwx.cn|sdtuxyh.cn|kngpdsz.cn|sugugaj.cn|mqqjrv.com)(\/.*)? url 302 $1long.$3$4
 ******************************************/
 [mitm] 
 
