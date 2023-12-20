bb73fc2f9cb1217f55f20baefb601f530c8a0bdd
atong <100357522+conghua11@users.noreply.github.com>
2023-12-20 21:01:45 +0800

    Update m3u8.js

--- m3u8.js
+++ m3u8.js
@@ -1,11 +1,10 @@
 [rewrite_local]
 
-^http[s]?:\/\/(h5play|10play|120play|long|sw-vcapi)\.(snowing|beibeini3|pili01a|fuhuida|riyufanyi|hncke).*\/(api|videos.|watch|static|upload|watch[0-9]+)\/.*\/.*\.m3u8\?auth_key.*$ url script-request-header https://raw.githubusercontent.com/conghua11/QuantumultX/main/m3u8.js
-^http[s]?:\/\/(sw\-vcapi)\.(hncke).*\/(api)\/.*\/.*\/.*\/.*\/.*\.m3u8\?sign.*$ url script-request-header https://raw.githubusercontent.com/conghua11/QuantumultX/main/m3u8.js
+^http[s]?:\/\/(h5play|10play|120play|long)\.(snowing|beibeini3|pili01a|fuhuida|riyufanyi|hncke).*\/(videos.|watch|static|upload|watch[0-9]+)\/.*\/.*\.m3u8\?auth_key.*$ url script-request-header https://raw.githubusercontent.com/conghua11/QuantumultX/main/m3u8.js
 
 # 通杀所有
 ^(https?:\/\/)(?!long)([^./]+\.)*(longyuandingyi.com|zihzot.com|snowing.ren|vzcnqmr.cn|beibeini3.cn|pili01a.net|hhclyjz.cn|bhhcdou.cn|xuxivwx.cn|sdtuxyh.cn|kngpdsz.cn|sugugaj.cn|mqqjrv.com)(\/.*)? url 302 $1long.$3$4
-  ^(https?:\/\/)(sw\-vcapi)*(hncke)(\/.*)? url 302 $1long.$3$4
+
 ******************************************/******************************************/
 [mitm] 
 
