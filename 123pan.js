d56fb596334c015c2647f2487301cd97e733d3cc
atong <100357522+conghua11@users.noreply.github.com>
2024-05-05 16:08:20 +0800

    Create 123pan.js

---
+++ 123pan.js
@@ -0,0 +1,29 @@
+#!name = 123yunpan
+#!desc = 测试本地VIP
+#!author = 小白
+#!update = 2024-05-05 16:00
+
+# 解锁VIP
+^https:\/\/www\.123pan\.com\/b\/api\/share\/download\/info url script-response-body https://raw.githubusercontent.com/conghua11/QuantumultX/main/123pan.js
+
+hostname = 123pan.com
+
+
+$task.fetch({
+    httpRequest: $resquest,
+    handler: function (response){
+        if (response.statusCode === 200){
+            var data = JSON.parse(response.body);
+            if ('data.data.Vip' in data) {
+                data['data']['Vip'] = true;
+                data['data']['VipExpire'] = '2099-12-31'
+            }
+            $done({
+                body:JSON.stringify(data)
+            })
+        }else {
+            console.log('解锁失败', response.statusCode)
+            $done
+        }
+    }
+})
