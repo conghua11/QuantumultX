ea48a548396855d09921f9d42648eecbfbdc628b
atong <100357522+conghua11@users.noreply.github.com>
2024-05-10 11:37:27 +0800

    Create 123.js

---
+++ 123.js
@@ -0,0 +1,56 @@
+/**************************************
+
+#!name = 123yunpan
+#!desc = 测试本地VIP
+#!author = 小白
+#!date = 2024-05-10
+[rewrite_local]
+^https:\/\/www\.123pan\.com\/api\/user\/info\?auth-key=\.*? url script-response-body https://raw.githubusercontent.com/conghua11/QuantumultX/main/123pan.js
+[mitm]
+hostname = www.123pan.com
+
+[Script]
+http-response ^https:\/\/www\.123pan\.com\/api\/user\/info\?auth-key=\.*? response-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/123pan.js
+[Mitm]
+hostname = www.123pan.com
+
+**************************************/
+
+
+var data = $response.body
+console.log('开始运行:\n',data)
+var body = JSON.parse($response.body)
+body.data ={
+    "IsShowAdvertisement" : false,
+        "VipExplain" : "Svip",
+        "SpaceTemp" : 7,
+        "Mail" : "",
+        "UserVipDetailInfos" : [
+
+    ],
+        "SpacePermanent" : 2199023255552,
+        "FileCount" : 242,
+        "ContinuousPaymentDate" : "2099-01-01 08:00:00",
+        "IsAuthentication" : true,
+        "SpaceUsed" : 183317505265,
+        "ContinuousPaymentAmount" : 5,
+        "VipLevel" : 5,
+        "UID" : 1636340,
+        "GrowSpaceAddCount" : 5,
+        "IsBeforeBuyVipProduct" : true,
+        "BindWechat" : false,
+        "ContinuousPaymentDuration" : 5,
+        "SpaceTempExpr" : "2099-01-01T00:00:00+00:00",
+        "Nickname" : "小白测试",
+        "HeadImage" : "https://statics.123pan.com/static-by-custom/default_avatar.png",
+        "StraightLink" : true,
+        "VipExpire" : "2099-01-01 08:00:00",
+        "ContinuousPayment" : true,
+        "SpaceBuy" : true,
+        "SignType" : 1,
+        "Vip" : true,
+        "Passport" : 13022632289,
+        "OpenLink" : 0
+},
+$done({body:JSON.stringify(body)})
+console.log('运行结束')
