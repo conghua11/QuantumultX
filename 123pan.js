3f590dd881da9979b19c2f6d525e2c6c65e9c249
atong <100357522+conghua11@users.noreply.github.com>
2024-05-10 10:36:19 +0800

    Update 123pan.js

--- 123pan.js
+++ 123pan.js
@@ -1,3 +1,4 @@
+/**************************************
 #!name = 123yunpan
 #!desc = 测试本地VIP
 #!author = 小白
@@ -11,6 +12,7 @@ hostname = www.123pan.com
 http-response ^https:\/\/www\.123pan\.com\/api\/user\/info\?auth-key=\.*? response-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/123pan.js
 [Mitm]
 hostname = www.123pan.com
+**************************************/
 var body = JSON.parse($response.body)
 body.data ={
     "IsShowAdvertisement" : false,
