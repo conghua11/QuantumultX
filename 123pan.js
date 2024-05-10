6f5184a37a484a97015be469bb0eb792b22fb73a
atong <100357522+conghua11@users.noreply.github.com>
2024-05-10 11:56:21 +0800

    Update 123pan.js

--- 123pan.js
+++ 123pan.js
@@ -1,26 +1,23 @@
 /**************************************
 
-#!name = 123yunpan
-#!desc = 测试本地VIP
+#!name = 123云盘
+#!desc = 解锁本地VIP
 #!author = 小白
 #!date = 2024-05-10
-
 [rewrite_local]
 ^https:\/\/www\.123pan\.com\/api\/user\/info\?auth-key=\.*? url script-response-body https://raw.githubusercontent.com/conghua11/QuantumultX/main/123pan.js
-
 [mitm]
 hostname = www.123pan.com
 
 [Script]
-http-response ^https:\/\/www\.123pan\.com\/api\/user\/info\?auth-key=\.*? response-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/123pan.js
-
+http-response ^https:\/\/www\.123pan\.com\/api\/user\/info\?auth-key=\.*? requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/123pan.js
 [Mitm]
 hostname = www.123pan.com
 
 **************************************/
 
-var data = $response.body
-console.log('开始运行:')
+
+console.log('开始运行!!!')
 var body = JSON.parse($response.body)
 body.data ={
     "IsShowAdvertisement" : false,
@@ -30,9 +27,9 @@ body.data ={
         "UserVipDetailInfos" : [
 
     ],
-        "SpacePermanent" : 2199023255552,
+        "SpacePermanent" : 10995116277760,
         "FileCount" : 242,
-        "ContinuousPaymentDate" : "2299-12-01 08:00:00",
+        "ContinuousPaymentDate" : "2099-01-01 08:00:00",
         "IsAuthentication" : true,
         "SpaceUsed" : 183317505265,
         "ContinuousPaymentAmount" : 5,
@@ -42,11 +39,11 @@ body.data ={
         "IsBeforeBuyVipProduct" : true,
         "BindWechat" : false,
         "ContinuousPaymentDuration" : 5,
-        "SpaceTempExpr" : "2299-12-01T08:00:00+00:00",
-        "Nickname" : "小白测试",
+        "SpaceTempExpr" : "2099-01-01T00:00:00+00:00",
+        "Nickname" : "小白解锁",
         "HeadImage" : "https://statics.123pan.com/static-by-custom/default_avatar.png",
         "StraightLink" : true,
-        "VipExpire" : "2299-12-01 08:00:00",
+        "VipExpire" : "2099-01-01 08:00:00",
         "ContinuousPayment" : true,
         "SpaceBuy" : true,
         "SignType" : 1,
@@ -54,5 +51,5 @@ body.data ={
         "Passport" : 13022632289,
         "OpenLink" : 0
 },
-console.log('运行结束')
+console.log('解锁成功!!!')
 $done({body:JSON.stringify(body)})
