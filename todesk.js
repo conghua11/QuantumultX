c3c43962b8084449192c2398d51c1a7f390aa433
atong <100357522+conghua11@users.noreply.github.com>
2024-05-14 21:18:45 +0800

    Update and rename kuwo.js to todesk.js

---
+++ todesk.js
@@ -0,0 +1,23 @@
+/****************************
+
+#!name = ToDesk
+#!desc = 解锁本地VIP
+#!author = 小白
+#!date = 2024-05-14
+
+[Script]
+# 迅雷
+http-response https://uc.todesk.com/api/user/public_info requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/todesk.js
+[Mitm]
+hostname = uc.todesk.com
+****************************/
+
+console.log('开始运行!!!')
+const body = JSON.parse($response.body);
+if (body.data && body.data.userConfigObj) {
+    body.data.userConfigObj['2'].end_time = 4099813930
+    console.log('解锁成功!!!')
+} else {
+    console.log('参数不存在,解锁失败!!!')
+}
+$done({body: JSON.stringify(body)});
