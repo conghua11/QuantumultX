3dce90b99d619b8d8bd705da1043e53e5315656c
atong <100357522+conghua11@users.noreply.github.com>
2024-05-16 10:03:41 +0800

    Create lsdj.js

---
+++ lsdj.js
@@ -0,0 +1,28 @@
+/***********************************
+ #!name = 乐刷短剧
+ #!desc = 解锁本地VIP
+ #!author = 小白
+ #!data = 2024-05-16
+
+ [Script]
+ http-response https://akdj.lunlunkj.vip/api/v1/user/account/info requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/lsdj.js
+ [Mitm]
+ hostname = akdj.lunlunkj.vip
+ ***********************************/
+const body  = JSON.parse($response.body)
+if (body.data) {
+    console.log('开始运行!!!')
+    body.data.vipInfo = {
+        vipType: 1,
+        id: body.data.id,
+        startTime: 1630000000000,
+        endTime: 4099766400000,
+        updataTime: 1630000000000
+    }
+    body.data.vipEnabled = true
+    console.log('解锁成功!!!')
+    $done({body: JSON.stringify(body)})
+}else {
+    console.log('解锁失败!!!')
+    $done({body: JSON.stringify(body)})
+}
