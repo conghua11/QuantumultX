c8d8080d8833ce876c785541179f62d8d964bef3
atong <100357522+conghua11@users.noreply.github.com>
2024-05-19 21:33:55 +0800

    Update pikpak.js

--- pikpak.js
+++ pikpak.js
@@ -7,7 +7,7 @@
 
 [Script]
 # PIKPAK
-http-response https://api-drive.mypikpak.com/(vip/v1/vip/info?|drive/v1/about?) requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/pikpak.js, tag=PIKPAK
+http-response https://api-drive.mypikpak.com/(vip/v1/vip/info?|drive/v1/about?|vip/v1/space/list?) requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/pikpak.js, tag=PIKPAK
 
 [Mitm]
 hostname = api-drive.mypikpak.com
@@ -44,5 +44,13 @@ if (url.indexOf('/drive/v1/about?') !== -1) {
     const body = JSON.parse($response.body);
     body.quota.limit = '10995116277760'
     console.log('解锁成功')
+    $done({body: JSON.stringify(body)});
+}
+if (url.indexOf('vip/v1/space/list?') !== -1) {
+    const body.base.assets = '10T'
+    const body.base.expire_time = "2099-12-01T21:30:15+08:00"
+    const body.size = '10995116277760'
+    const body.vip_status = 'ok'
+    onsole.log('解锁成功')
     $done({body: JSON.stringify(body)});
 }
