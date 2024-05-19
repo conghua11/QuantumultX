617052d43afb299c5c813d2e57f0428885919f99
atong <100357522+conghua11@users.noreply.github.com>
2024-05-19 23:18:16 +0800

    Update pikpak.js

--- pikpak.js
+++ pikpak.js
@@ -7,7 +7,7 @@
 
 [Script]
 # PIKPAK
-http-response https://api-drive.mypikpak.com/(vip/v1/vip/info?|drive/v1/about?|vip/v1/space/list?|vip/v1/allSubscriptionStatus) requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/pikpak.js, tag=PIKPAK
+http-response ^https:\/\/api-drive\.mypikpak\.com\/(vip\/v\d\/(vip\/info|allSubscriptionStatus|space\/list)|drive\/v\d\/about\?space) requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/pikpak.js, tag=PIKPAK
 
 [Mitm]
 hostname = api-drive.mypikpak.com
@@ -16,7 +16,11 @@ hostname = api-drive.mypikpak.com
 console.log('开始运行!!!')
 const req = $request;
 const url = $request.url;
-if (url.indexOf('/vip/v1/vip/info?') !== -1) {
+const URL1 = '/vip/info';
+const URL2 = '/about';
+const URL3 = '/space/list'
+const URL4 = '/allSubscriptionStatus';
+if (url.indexOf(URL1) !== -1) {
     const body = JSON.parse($response.body);
     body.data.expire = "2099-12-01T10:12:29+08:00"
     body.data.status = "ok"
@@ -33,13 +37,13 @@ if (url.indexOf('/vip/v1/vip/info?') !== -1) {
     console.log('解锁成功')
     $done({body: JSON.stringify(body)});
 }
-if (url.indexOf('/drive/v1/about?') !== -1) {
+if (url.indexOf(URL2) !== -1) {
     const body = JSON.parse($response.body);
     body.quota.limit = '10995116277760'
     console.log('解锁成功')
     $done({body: JSON.stringify(body)});
 }
-if (url.indexOf('/vip/v1/space/list?') !== -1) {
+if (url.indexOf('URL3') !== -1) {
     const body = JSON.parse($response.body);
     body.base.assets = '10T'
     body.base.expire_time = "2099-12-01T21:30:15+08:00"
@@ -48,7 +52,7 @@ if (url.indexOf('/vip/v1/space/list?') !== -1) {
     console.log('解锁成功')
     $done({body: JSON.stringify(body)});
 }
-if (url.indexOf('/vip/v1/allSubscriptionStatus') !== -1) {
+if (url.indexOf(URL4) !== -1) {
     const body = JSON.parse($response.body);
     body.apple = {"subscribed":true,
                   "purchased":true,
