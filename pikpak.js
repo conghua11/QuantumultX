c25e843cd5fb447580d18786a9cf1bdf4ea1143a
atong <100357522+conghua11@users.noreply.github.com>
2024-05-19 16:08:42 +0800

    Update pikpak.js

--- pikpak.js
+++ pikpak.js
@@ -7,29 +7,35 @@
 
 [Script]
 # PIKPAK
-http-response https://api-drive.mypikpak.com/vip/v1/vip/info? requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/pikpak.js, tag=PIKPAK
+http-response https://api-drive.mypikpak.com/(vip/v1/vip/info? | drive/v1/about?space=) requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/pikpak.js, tag=PIKPAK
+
 [Mitm]
 hostname = api-drive.mypikpak.com
 ****************************/
 
 const body = JSON.parse($response.body);
-body.data.expire = "2099-12-01T10:12:29+08:00"
-body.data.status = "ok"
-body.data.type = "platinum"
-body.data.vipItem = [
-            {
-                "type": "global",
-                "description": "全球会员",
-                "status": "ok",
-                "expire": "2099-12-01T21:30:15+08:00",
-                "surplus_day": 9999
-            },
-            {
-                "type": "regional",
-                "description": "区域会员",
-                "status": "ok",
-                "expire": "2099-12-01T10:12:29+08:00",
-                "surplus_day": 9999
-            }
-        ]
-$done({body: JSON.stringify(body)});
+if (url.indexOf('/vip/v1/vip/info?') !== -1) {
+    body.data.expire = "2099-12-01T10:12:29+08:00"
+    body.data.status = "ok"
+    body.data.type = "platinum"
+    body.data.vipItem = [
+        {
+            "type": "global",
+            "description": "全球会员",
+            "status": "ok",
+            "expire": "2099-12-01T21:30:15+08:00",
+            "surplus_day": 9999
+        },
+        {
+            "type": "regional",
+            "description": "区域会员",
+            "status": "ok",
+            "expire": "2099-12-01T10:12:29+08:00",
+            "surplus_day": 9999
+        }
+    ]
+    $done({body: JSON.stringify(body)});
+}
+if (url.indexOf('/drive/v1/about?space=') !== -1) {
+    body.quota.limit = '10995116277760'
+    $done({body: JSON.stringify(body)});
