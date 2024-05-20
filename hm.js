2f0b7f81b003c5ceba514a0d1310346aa0b73ff5
atong <100357522+conghua11@users.noreply.github.com>
2024-05-20 09:09:37 +0800

    更新 hm.js

--- hm.js
+++ hm.js
@@ -1,8 +1,12 @@
 /*
 [Script]
-http-response https://businessapi.hprtupgrade.com/api/tem.app_member/getMemberUser response-body=1,script-path=
+http-response https://businessapi.hprtupgrade.com/api/tem.app_member/getMemberUser requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/hm.js
+[Mitm]
+hostname = businessapi.hprtupgrade.com
 */
+const body = JSON.parse($response.body)
 body.data.status = "ok"
 body.data.is_member = true
 body.data.available_space = 20
 body.data.expire_time = "2099-12-01"
+$done(
