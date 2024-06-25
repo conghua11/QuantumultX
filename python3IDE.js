4de3db43aedb9e1e2fa365e5c090d1e6f7d747b7
atong <100357522+conghua11@users.noreply.github.com>
2024-06-25 18:15:04 +0800

    Update python3IDE.js

--- python3IDE.js
+++ python3IDE.js
@@ -19,25 +19,25 @@ const body = JSON.parse($response.body)
 body.original_application_version = "365"
 body.subscriptions = {
     "python3ide_annual": {
-        "original_purchase_date": "2024-06-25T00:42:46Z",
+        "original_purchase_date": "2099-12-01T00:42:45Z",
         "expires_date": "2099-12-01T00:42:45Z",
         "is_sandbox": true,
-        "refunded_at": null,
+        "refunded_at": '2099-12-01T00:42:45Z',
         "store_transaction_id": "280001927741086",
-        "unsubscribe_detected_at": "2024-06-25T00:45:53Z",
-        "grace_period_expires_date": null,
+        "unsubscribe_detected_at": "2099-12-01T00:42:45Z",
+        "grace_period_expires_date": '2099-12-01T00:42:45Z',
         "period_type": "formal",
-        "purchase_date": "2024-06-25T00:42:45Z",
-        "billing_issues_detected_at": null,
+        "purchase_date": "2099-12-01T00:42:45Z",
+        "billing_issues_detected_at": '2099-12-01T00:42:45Z',
         "ownership_type": "PURCHASED",
         "store": "app_store",
-        "auto_resume_date": null
+        "auto_resume_date": '2099-12-01T00:42:45Z'
     }
 }
 body.entitlements = {
     "pro": {
-        "grace_period_expires_date": null,
-        "purchase_date": "2024-06-25T00:42:45Z",
+        "grace_period_expires_date": '2099-12-01T00:42:45Z',
+        "purchase_date": "2099-12-01T00:42:45Z",
         "product_identifier": "python3ide_annual",
         "expires_date": "2099-12-01T00:42:45Z"
     }
