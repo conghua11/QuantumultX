1abfc8dfe8399a8d703f2a4741f8b206474ec61d
atong <100357522+conghua11@users.noreply.github.com>
2024-07-02 21:38:59 +0800

    Update pkhd.js

--- pkhd.js
+++ pkhd.js
@@ -21,14 +21,15 @@ hostname = *.mypikpak.com
 function generateRandomIP() {
   return `${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}.${Math.floor(Math.random() * 256)}`;
 }
-
+const ip = generateRandomIP(10)
 // 获取当前请求
 const request = $request;
 
 // 添加或修改请求头
-request.headers['x-forwarded-for'] = generateRandomIP(10);
+request.headers['x-forwarded-for'] = ip;
+request.hraders['X-Real-IP'] = ip;
 if(request.headers['x-forwarded-for']){
-    console.log('生成随机IP:', request.headers['x-forwarded-for'])
+    console.log('生成随机IP:', ip)
 }
 // 返回修改后的请求
 $done({ headers: request.headers });
