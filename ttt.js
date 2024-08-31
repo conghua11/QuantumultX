a97bd8062054373d9c6cfdac0db3ed1a2d4c8182
atong <100357522+conghua11@users.noreply.github.com>
2024-09-01 00:40:15 +0800

    更新 ttt.js

--- ttt.js
+++ ttt.js
@@ -11,5 +11,5 @@ http-request ^https:\/\/[^\/]*\.shoknkj\.cn\/\w+\/[a-z0-9]{32}\/[a-z0-9]{32}\.m3
 http-request https:\/\/[^\/]*\..*\.cn\/\w+\/[a-z0-9]{32}\/[a-z0-9]{32}\.m3u8(\?.*)? script-path=https://raw.githubusercontent.com/Yu9191/Rewrite/main/lsp/Tangtoutiao.js
 
 [MITM]
-hostname = *.shjsvkj.cn, *.qxoovv.cn, *.shoknkj.cn, *.shedbgs.cn, *.sholfgs.cn
+hostname = *.shjsvkj.cn, *.qxoovv.cn, *.shoknkj.cn, *.shedbgs.cn, *.shnyigs.cn, *.sholfgs.cn, *.shjtqgs.cn, *.shjtpkj.cn, *.hfsudf.cn
 **************************************/
