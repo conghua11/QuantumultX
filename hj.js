117f2a7eae21b8fef91f1f413daad154f764c955
atong <100357522+conghua11@users.noreply.github.com>
2024-12-11 14:55:49 +0800

    Update hj.js

--- hj.js
+++ hj.js
@@ -14,7 +14,7 @@ http-request https://api.iamok.in/api/Support/GetRuntimeData requires-body=1, ma
 hostname = api.iamok.in
 **************************************/
 
-const body =JSON.parse($requests.body)
+const body =JSON.parse($request.body)
 body.token = 'MMJ2tC/zmghIHZBPB7qS6NWTYBrLQOoSNAUM/3PAeHomGJYutJ9Vj1KSmg+eRB8H7V2x17+aWSUo8Fqi53u/RvqJSB3TpoUIJXVTDDmJoDr0ANH4Hvj1z+QDUmYGyXjWLk+PNLiW7Nf8OEzwYOWkuSNonr6ZA/2mk2pxxa3N0sOZKMVNURXNYzQYXiy49/VMZNO0fkdpZMCA26ELLJvpyw==
 console.log('测试无限试用')
 $done({ body: JSON.stringify(body) })
