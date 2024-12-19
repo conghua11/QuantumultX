84387ec2009bafaed503de2b81861882dd394cb5
atong <100357522+conghua11@users.noreply.github.com>
2024-12-19 16:34:38 +0800

    Update hj.js

--- hj.js
+++ hj.js
@@ -1,20 +1,22 @@
 /**************************************
 #!name = 画镜
-#!desc = 测试试看
+#!desc = 测试
 #!author = 小白
-#!date = 2024-12-11
+#!date = 2024-12-19
 
 
 [Script]
 
-http-request https://api.iamok.in/api/Support/GetRuntimeData requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/hj.js, tag=测试
+http-request https://api.iamok.in/api/Sn/QuerySn?sn= requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/conghua11/QuantumultX/main/hj.js, tag=测试
 
 [MITM]
 
 hostname = api.iamok.in
 **************************************/
 
-const body =JSON.parse($request.body)
-body.token = 'MMJ2tC/zmghIHZBPB7qS6NWTYBrLQOoSNAUM/3PAeHomGJYutJ9Vj1KSmg+eRB8H7V2x17+aWSUo8Fqi53u/RvqJSB3TpoUIJXVTDDmJoDr0ANH4Hvj1z+QDUmYGyXjWLk+PNLiW7Nf8OEzwYOWkuSNonr6ZA/2mk2pxxa3N0sOZKMVNURXNYzQYXiy49/VMZNO0fkdpZMCA26ELLJvpyw=='
-console.log('测试无限试用')
+
+const body = {"ID": "7593", "Kn": "e9813ff6-667c-4a38-99cf-e77454be1f02", "Pt": "allvideo-3650", "Ds": "3650",
+        "St": "2024-12-19 16:24:00", "Dd": false, "Note": "电报分享", "CreateTime": "2024-12-14 00:00:01",
+        "OwnerCode": "黑猫", "Status": "已激活", "Price": "13.8", "PriceUnit": "RMB"}
+console.log('解锁测试')
 $done({ body: JSON.stringify(body) })
